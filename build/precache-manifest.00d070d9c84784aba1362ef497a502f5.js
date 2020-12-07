93685aa6ea783875c4ea7fa60a10f66a7e96e60e
JoshuaParkSJ <jpark9061@gmail.com>
2020-12-06 18:41:16 -0800

    feat: initial build for GAE deploy

---
+++ build/precache-manifest.00d070d9c84784aba1362ef497a502f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3916ec4b185957a1c086c480a6775213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64bb877bb5df0652415d",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "e9673c0fe8aa9268b9ca",
+    "url": "/static/css/main.04f4f6ad.chunk.css"
+  },
+  {
+    "revision": "64bb877bb5df0652415d",
+    "url": "/static/js/2.c9a50bbe.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "/static/js/2.c9a50bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9673c0fe8aa9268b9ca",
+    "url": "/static/js/main.796d7751.chunk.js"
+  },
+  {
+    "revision": "e5eab0c7f677da775e00",
+    "url": "/static/js/runtime-main.c216ca56.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
